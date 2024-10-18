a2a9eeeb94cf6c3c5a2a7af674c1990e068ced88
Eduardo Fermiano <dufermiano43@gmail.com>
2024-10-18 16:50:40 -0300

    chore: adding nodemon and cors to project

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import sequelize from './database';
 import { PesquisaController } from './controllers/PesquisaController';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 const pesquisaController = new PesquisaController();
 
@@ -16,11 +18,11 @@ app.post('/pesquisas', (req: Request, res: Response) => {
   pesquisaController.criarPesquisa(req, res);
 });
 
-sequelize.sync().then(() => {
+sequelize.sync({ alter: true, force: true }).then(() => {
   console.log('Conexão com o banco de dados estabelecida.');
 
   app.listen(3001, () => {
-    console.log('Servidor rodando na porta 3000');
+    console.log('Servidor rodando na porta 3001');
   });
 }).catch((err: Error) => {
   console.error('Erro ao conectar com o banco de dados:', err);
